1e3eaf6498b16c5e7627a886646c87ffff0777fa
Theo-Borg <theo.borg2@etu.univ-lorraine.fr>
2019-01-10 11:50:27 +0100

    modification seed/migration mushroom + controller/blade mushroom + ajout FPDF

---
+++ app/Http/Controllers/ListeChampiController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
+class ListeChampiController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($id)
+    {   
+        $liste = DB::table('Mushroom')->where('id', $id)->get();
+        $edibility = DB::table('Edibility')->where('id', $liste[0]->edibility)->get();
+        
+        return view('listeChampi', compact('mushroom'), compact('edibility'));
+    }
+}
